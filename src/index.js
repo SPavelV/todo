13f5df02acf3a943b8e412bbb01f234dd012c21e
PavelSolovev <pashius2010@gmail.com>
2019-06-03 23:38:00 +0400

    start project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
-console.log('Hello World!')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const ToDOList = () => {
+  const items = ['Drink coffee', 'Build Awesome to-do App']
+
+  return (
+    <ul>
+        <li>{items[0]}</li>
+        <li>{items[1]}</li>
+      </ul>
+  )
+}
+
+const AppHeader = () => {
+  return (
+    <h1>My to-do list.</h1>
+  )
+}
+
+const SearchPanel = () => {
+  const searchText = 'Type here to search'
+  const searchStyle = {
+    fontSize: '25px'
+  }
+  return <input 
+              placeholder={searchText} 
+              style = {searchStyle}
+              disabled
+              type="text"/>
+}
+
+const App = () => {
+
+  return (
+    <div>
+      <AppHeader/>
+      <SearchPanel/>
+      <ToDOList/>
+    </div>
+  )
+}
+  
+ReactDOM.render(<App/>, document.getElementById('root'))
